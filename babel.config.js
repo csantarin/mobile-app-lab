cbab1c502b3d57dbb9accf7591d7ffb92bba9811
Cezar Andrew Villegas Santarin <andrew.santarin@gmail.com>
2022-03-02 21:31:06 +0800

    build(npm): enable absolute imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver', {
+        root: ['./src'],
+        extensions: [
+          '.android.js',
+          '.android.ts',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.ts',
+          '.ios.tsx',
+          '.js',
+          '.json',
+          '.ts',
+          '.tsx',
+        ],
+      },
+    ],
+    'jest-hoist',
+  ],
 };
