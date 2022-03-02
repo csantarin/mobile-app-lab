9354bbbdece9f7e37bad3510e8e27854e246056f
Cezar Andrew Villegas Santarin <andrew.santarin@gmail.com>
2022-03-03 02:26:28 +0800

    refactor(main): reorder Main.tsx imports

--- src/modules/main/tests/Main.spec.tsx
+++ src/modules/main/tests/Main.spec.tsx
@@ -1,10 +1,8 @@
 import 'react-native';
 import React from 'react';
+import renderer from 'react-test-renderer';
 
 import Main from '../views/Main';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   renderer.create(<Main />);
